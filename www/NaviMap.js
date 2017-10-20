bcab55cea8d3c3a7ecb1aeb5e7807dca29264506
石头 <hupei132@qq.com>
2017-10-20 08:58:35 +0800

    修复bug

--- www/NaviMap.js
+++ www/NaviMap.js
@@ -3,11 +3,11 @@ var exec = require('cordova/exec');
 var api = {};
 
 api.amapRoute = function(uri, success, error) {
-    exec(success, error, "Navi", "amapRoute", [uri]);
+    exec(success, error, "NaviMap", "amapRoute", [uri]);
 };
 
 api.bdmapRoute = function(uri, success, error) {
-    exec(success, error, "Navi", "bdmapRoute", [uri]);
+    exec(success, error, "NaviMap", "bdmapRoute", [uri]);
 };
 
 module.exports = api;
